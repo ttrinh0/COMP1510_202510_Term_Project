6f0ea2b0b3b454852347dd933d257d3dad97b89e
TIN TRINH <tin.ttrinh0@gmail.com>
2025-04-02 21:41:01 -0700

    Finish fish_collection fish names

--- setup.py
+++ setup.py
@@ -106,15 +106,21 @@ def make_fish_collection():  # REVISE FISH NAMES AND DESC
 
     :return:
     """
-    fish_collection_area_one = {1: ('Catfish', 'fish desc'), 2: ('fish name', 'fish desc'),
-                                3: ('Neon Tetra', 'fish desc')}
-    fish_collection_area_two = {4: ('Dogfish', 'Weird dog, er-- fish.'), 5: ('fish name', 'fish desc'),
-                                6: ('fish name', 'fish desc')}
-    fish_collection_area_three = {7: ('Tuna', "Can't piano a tuna, but you can tuna piano! Hey, what's this glue doing "
-                                              "here?"), 8: ('Bigfish', "It's big, it's blue, it's... incredibly shiny"),
-                                  9: ('fish name', 'fish desc'), 10: ('fish name', 'fish desc')}
+    fish_collection_area_one = {1: ('Goldfish', 'BUY GOLD!'),
+                                2: ('Guppy', 'A tiny little fella. A little, little fish.'),
+                                3: ('Neon Tetra', 'A small fish with a bright line to light up your life.')}
+    fish_collection_area_two = {4: ('Dogfish', 'Weird dog, er-- fish.'),
+                                5: ('Catfish', 'It\'s hissing at you'),
+                                6: ('River Trout', 'How does this affect the trout population?')}
+    fish_collection_area_three = {7: ('Tuna', "Can't piano a tuna, but you can tuna piano! Hey, what's this glue doing"
+                                              " here?"),
+                                  8: ('Bigfish',
+                                      "Small in size, but big in heart. It's blue, and it's... incredibly shiny"),
+                                  9: ('Flying Fish', 'It practically flew into your boat!'),
+                                  10: ('Shark', 'Doo doo doo, doo doo doo doo doo.')}
+    final_fish = {11: ('Final Fishtasy', 'The legendary fish... and now it\'s in your bucket.')}
 
-    return fish_collection_area_one, fish_collection_area_two, fish_collection_area_three
+    return fish_collection_area_one, fish_collection_area_two, fish_collection_area_three, final_fish
 
 
 def create_game_parameters():
