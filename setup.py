0da769e7c958dde195cdaa8881d3b2d2235aba63
TIN TRINH <tin.ttrinh0@gmail.com>
2025-04-06 19:08:02 -0700

    Create doctest for make_fish_collection function

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ def choose_rod(character: dict) -> str:
     return rod
 
 
-def make_fish_collection():  # REVISE FISH NAMES AND DESC
+def make_fish_collection() -> tuple:  # REVISE FISH NAMES AND DESC
     """
     Create a tuple containing a dictionary of the collection of fish you can catch in each area.
 
@@ -192,6 +192,16 @@ def make_fish_collection():  # REVISE FISH NAMES AND DESC
     :postcondition: creates a tuple containing the dictionaries of the collection of fish you can catch in each area
     :return fish_collection_area_one, fish_collection_area_two, fish_collection_area_three, final_fish: four
                     dictionaries contained within a tuple
+    >>> fish_collection = make_fish_collection()
+    >>> fish_collection == ({1: ('Goldfish', 'BUY GOLD!'), 2: ('Guppy', 'A tiny little fella. A little, little fish.'),
+    ... 3: ('Neon Tetra', 'A small fish with a bright line to light up your life.')},
+    ... {4: ('Dogfish', 'Weird dog, er-- fish.'), 5: ('Catfish', "It's hissing at you..."),
+    ... 6: ('River Trout', 'How does this affect the trout population?')},
+    ... {7: ('Tuna', "Can't piano a tuna, but you can tuna piano! Hey, what's this glue doing here?"),
+    ... 8: ('Bigfish', "Small in size, but big in heart. It's blue, and it's... incredibly shiny"),
+    ... 9: ('Flying Fish', 'It practically flew into your boat!'), 10: ('Shark', 'Doo doo doo, doo doo doo doo doo.')},
+    ... {11: ('Final Fishasy', "The legendary fish... and now it's in your bucket.")})
+    True
     """
     fish_collection_area_one = {1: ('Goldfish', 'BUY GOLD!'),
                                 2: ('Guppy', 'A tiny little fella. A little, little fish.'),
